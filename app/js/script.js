1719ae2509da9182029cfd80936f90ed5d1c3411
I.Smal <serbov@pochta.ru>
2017-02-15 20:40:49 +0300

    univercity is ready & some form changes

--- app/js/script.js
+++ app/js/script.js
@@ -228,28 +228,31 @@ $(document).ready(function() {
 	};
 	newsSwitcher.toggle();
 
-	//club form
-	var clubForm = {
-		"form": $('.js-club-form'),
-		"formOpenBtn": $('.js-club-form-open'),
-		"formCloseBtn": $('.js-club-form-close'),
-		"clubBlur": $('.js-club-blur'),
+	//form
+	var formPopup = {
+		"form": $('.js-form'),
+		"formOpenBtn": $('.js-form-open'),
+		"formCloseBtn": $('.js-form-close'),
+		"clubBlur": $('.js-blur'),
+		"footer": $('.footer'),
 		"run": function() {
 			var _this = this;
 			this.formOpenBtn.on('click', function(e) {
 				e.preventDefault();
 				_this.form.addClass('_show');
 				_this.form.fadeIn(300);
-				_this.clubBlur.addClass('_blur');
-				$('html').addClass('_unscroll');
+				_this.clubBlur.addClass('blur');
+				_this.footer.addClass('blur');
+				$('html').addClass('_unscroll-md');
 			});
 			this.formCloseBtn.on('click', function() {
 				_this.form.removeClass('_show');
 				_this.form.fadeOut(300);
-				$('html').removeClass('_unscroll');
-				_this.clubBlur.removeClass('_blur');
+				$('html').removeClass('_unscroll-md');
+				_this.clubBlur.removeClass('blur');
+				_this.footer.removeClass('blur');
 			});
 		}
 	};
-	clubForm.run();
+	formPopup.run();
 });
